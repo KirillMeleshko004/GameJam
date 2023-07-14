921643f4b7e0fd21cf3a5ad41a36fd8484655c86
Kirill Meleshko <meleshkokirill004@gmail.com>
2023-07-14 20:50:00 +0300

    Added blackout and character animation

--- GameJam/Assets/Scripts/Core/SceneChangers/TriggerSceneChanger.cs
+++ GameJam/Assets/Scripts/Core/SceneChangers/TriggerSceneChanger.cs
@@ -1,19 +1,57 @@
 using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Threading;
+using System.Collections;
 
 namespace GameJam.Core.SceneChangers
 {
     //Scene changer, which changing scene when player enters designated zone
     public class TriggerSceneChanger : BaseSceneChanger, ITriggerSceneChanger
     {
-        private void ChangeScene()
-        {
-            SceneManager.LoadScene(_targetSceneBuildID);
-        }
+        #region Variables
+        [SerializeField]
+        private Animator _currentAnimator;
 
+        [SerializeField]
+        private float _fadeInAnimTime = 1f;
+        [SerializeField]
+        private float _fadeOutAnimTime = 3f;
+        #endregion
+
+        #region Built-in methods
         private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
         {
-            if(collision.transform.root.CompareTag("Player"))
-                ChangeScene();
+            if (collision.transform.root.CompareTag("Player"))
+                StartCoroutine(LoadSceneWithAnimation());
         }
+        #endregion
+
+        #region Custom methods
+        private void ChangeScene()
+        {
+            _currentAnimator.SetTrigger("FadeIn");
+            Debug.Log(_currentAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+
+            SceneManager.LoadScene(_targetSceneBuildID);
+            //while (true)
+            //{
+            //    Debug.Log(_blackoutAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+            //    if(_blackoutAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1f)
+            //    {
+            //        SceneManager.LoadScene(_targetSceneBuildID);
+            //        Debug.Log(_blackoutAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+            //        break;
+            //    }
+            //}
+
+        }
+
+        private IEnumerator LoadSceneWithAnimation()
+        {
+            _currentAnimator.SetTrigger("FadeIn");
+            yield return new WaitForSeconds(_fadeOutAnimTime);
+            SceneManager.LoadScene(_targetSceneBuildID);
+        }
+        #endregion
     }
 }
