387a69578147c6240e386fdbddd0981e54eebc3b
Kirill Meleshko <meleshkokirill004@gmail.com>
2023-07-15 15:02:11 +0300

    Added menu

---
+++ GameJam/Assets/Scripts/Core/UI/MainMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GameJam.Core.UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+       public void StartGame()
+       {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       }
+    }
+}
