a269790114cfd021cc2d6d3b0d75c42cabed7c81
Kirill Meleshko <meleshkokirill004@gmail.com>
2023-07-16 11:16:55 +0300

    Some kind of refactor on calculator and fix some bugs

--- GameJam/Assets/Scripts/Excel Minigame/Calculator.cs
+++ GameJam/Assets/Scripts/Excel Minigame/Calculator.cs
@@ -1,9 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 namespace GameJam.ExcelMinigame
 {
+    enum Operations
+    {
+        Plus,
+        Minus,
+        Divide,
+        Multiply,
+        Empty
+    }
     public class Calculator : MonoBehaviour
     {
         #region Variables
@@ -11,66 +17,130 @@ namespace GameJam.ExcelMinigame
         TMP_InputField _inputField;
 
         private float _firstValue;
-        private float _secondValue;
-        private float _result;
-        private string _operation;
+        private float? _result = null;
+        private Operations _operation = Operations.Empty;
         #endregion
 
         #region Properties
         //Binded to display
         public string ResultString { get; set; } = "0";
+
         #endregion
 
 
         #region Custom Methods
         public void OnNumberClick(int number)
         {
-            if(_inputField.text == "0")
+            if (_result is not null || _inputField.text == "0")
             {
-                _inputField.text = string.Empty;
+                ClearResult();
+                ClearEntry();
             }
-            _inputField.text += number;
-            Debug.Log(ResultString);
+
+            AddNumberOnDisplay(number);
         }
         public void OnOperationClick(string operation)
         {
-            _firstValue = float.Parse(ResultString);
-            _operation = operation;
-            _inputField.text = "0";
+            if (_operation != Operations.Empty)
+            {
+                float _secondValue = GetValueFromDisplay();
+                _firstValue = PerfomOperation(_operation, _firstValue, _secondValue);
+            }
+            else
+                _firstValue = GetValueFromDisplay();
+
+            NullifyEntry();
+            _operation = GetOperation(operation);
         }
         public void OnEqualsClick()
         {
-            if (_operation == string.Empty) return;
+            if (_operation == Operations.Empty) return;
 
-            _secondValue = float.Parse(ResultString);
-            
-            switch(_operation)
-            {
-                case "+": _result = _firstValue + _secondValue; break;
-                case "-": _result = _firstValue - _secondValue; break;
-                case "/": _result = _firstValue / _secondValue; break;
-                case "*": _result = _firstValue * _secondValue; break;
-            }
+            float _secondValue = GetValueFromDisplay();
 
-            _operation = "";
+            _result = PerfomOperation(_operation, _firstValue, _secondValue);
 
-            _inputField.text = _result.ToString();
+            ShowResult(_result);
 
-            _firstValue = _result;
+            ResetOperation();
         }
 
         public void OnClearClick()
         {
             _result = 0;
             _firstValue = 0;
-            _secondValue = 0;
-            _operation = "";
-            _inputField.text = "0";
+            _operation = Operations.Empty;
+            NullifyEntry();
         }
         public void OnClearEntryClick()
         {
+            NullifyEntry();
+        }
+
+
+        #region Auxiliary Methods
+        public void ClearEntry()
+        {
+            _inputField.text = string.Empty;
+        }
+
+        public void NullifyEntry()
+        {
             _inputField.text = "0";
         }
+
+        public float GetValueFromDisplay()
+        {
+            return float.Parse(ResultString);
+        }
+
+        private float PerfomOperation(Operations operation, float firstValue, float secondValue)
+        {
+            return operation switch
+            {
+                Operations.Plus => firstValue + secondValue,
+                Operations.Minus => firstValue - secondValue,
+                Operations.Divide => firstValue / secondValue,
+                Operations.Multiply => firstValue * secondValue,
+                _ => float.NaN,
+            };
+        }
+
+        private void ShowResult(float? result)
+        {
+            _inputField.text = result?.ToString();
+        }
+
+        private void AddNumberOnDisplay(int number)
+        {
+            _inputField.text += number.ToString();
+        }
+
+        private void ResetOperation()
+        {
+            _operation = Operations.Empty;
+        }
+
+        private Operations GetOperation(string sOperation)
+        {
+            return sOperation switch
+            {
+                "+" => Operations.Plus,
+                "-" => Operations.Minus,
+                "/" => Operations.Divide,
+                "*" => Operations.Multiply,
+                _ => Operations.Empty
+            };
+        }
+
+        private void ClearResult()
+        {
+            _result = null;
+        }
         #endregion
+
+        #endregion
+
+
     }
 }
